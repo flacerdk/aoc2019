45e57630ec6c799ee6232cb4fc2ffc4c8015b7ed
Felipe Lacerda <fegolac@gmail.com>
2019-12-06 15:58:47 +0100

    Add test for day 2

--- day2/day2.ml
+++ day2/day2.ml
@@ -1,8 +1,6 @@
 open Core
 
-let input =
-  let line = In_channel.read_all "day2.txt" |> String.strip in
-  String.split line ~on:',' |> Array.of_list |> Array.map ~f:Int.of_string
+let parse input_string = String.split input_string ~on:',' |> Array.of_list |> Array.map ~f:Int.of_string
 
 let step prog ip =
   (match prog.(ip) with
@@ -23,3 +21,15 @@ let rec run input noun verb =
   if output = 19690720
   then 100 * noun + verb
   else if verb >= 99 then (if noun >= 99 then -1 else run input (noun + 1) 0) else run input noun (verb + 1)
+
+let solve_1 input_string =
+  let input = parse input_string in
+  Int.to_string ((operate input 0).(0))
+
+let solve_2 input_string =
+  let input = parse input_string in
+  run input 0 0
+
+let%expect_test "Solve example" =
+  print_string (solve_1 "1,9,10,3,2,3,11,0,99,30,40,50");
+  [%expect {| 3500 |}]
